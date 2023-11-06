9b6e3fab4dfdac0720f240988e3c789d285ad495
Pupa06 <126087025+Pupa06@users.noreply.github.com>
2023-11-06 23:47:11 +0700

    update get manager và employee

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import UserController from '../controllers/user.controller.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 // Lấy thông tin tất cả người dùng
-router.get('/', UserController.getProfile);
+router.get('/', UserController.getAllUser);
+// Lấy thông tin tất cả quản lý
+router.get('/manager', UserController.getManager);
+// Lấy thông tin tất cả nhân viên
+router.get('/employee', UserController.getEmployee);
+// Lấy thông tin id người dùng
+router.get('/:id', authMiddleware, UserController.getProfileId);
 
 // Update thông tin người dùng
 router.put('/:id', UserController.updateProfile);
