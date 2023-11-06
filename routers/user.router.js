58dfccec3219ca63abed1cde17cd82e465718d7a
Pupa06 <126087025+Pupa06@users.noreply.github.com>
2023-11-06 22:16:59 +0700

    update api user

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,4 +1,19 @@
 import express from 'express';
-import UserModel from '../models/user.model';
+import UserController from '../controllers/user.controller.js';
 
 const router = express.Router();
+
+// Lấy thông tin tất cả người dùng
+router.get('/', UserController.getProfile);
+
+// Update thông tin người dùng
+router.put('/:id', UserController.updateProfile);
+
+// Xóa người dùng
+router.delete('/:id', UserController.deleteProfile);
+
+// Đổi mật khẩu người dùng
+router.put('/change-password/:id', UserController.changePassword);
+
+export default router;
+
