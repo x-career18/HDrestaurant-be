46dcb05969edfb3fe359f933d00347cacd044ff3
Pupa06 <126087025+Pupa06@users.noreply.github.com>
2023-11-20 00:30:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,17 +11,22 @@ const PORT = 5000;
 connectDB();
 // 2. Global middlewares
 app.use(express.json());
-app.use(cors("*"));
+app.use(cors({
+  origin: ['https://restaurantad.netlify.app/', 'https://restaurantclient.vercel.app/'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+}));
 app.use((req, res, next) => {
-    res.header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
-    res.header("Pragma", "no-cache");
-    next();
-  });
+  res.header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
+  res.header("Pragma", "no-cache");
+  next();
+});
 // 3. Routing
 app.use('/api/v1', router);
 // 4. Error handling
 app.use(errorHandlerMiddleware);
 // 5. Start server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
